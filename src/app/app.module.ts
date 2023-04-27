d4e1ba0c0f7ad8a96070a58dc0d2041931580c4a
Rodrigo <rodrigofbarrios2803@gmail.com>
2023-04-27 05:08:58 -0300

    #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +23,17 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     SocialComponent,
     BannerComponent,
     AcercaDeComponent,
-    EducacionComponent
+    EducacionComponent,
+    ExperienciaComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
